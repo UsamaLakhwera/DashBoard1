59114ec421b06bb5e0590f80456a01e5f4be6cb1
Usama Lakhwera <usama.lakhwera1@gmail.com>
2022-11-21 17:53:10 +0500

    initial commit

---
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/jsx-no-target-blank */
+
+import {useIntl} from 'react-intl'
+
+import {AsideMenuItem} from './AsideMenuItem'
+
+export function AsideMenuMain() {
+  const intl = useIntl()
+
+  return (
+    <>
+      <AsideMenuItem
+        to='/dashboard'
+        icon='/media/icons/duotune/art/art002.svg'
+        title={intl.formatMessage({id: 'MENU.DASHBOARD'})}
+        fontIcon='bi-app-indicator'
+      />
+      
+      
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <div className='separator mx-1 my-4'></div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/_metronic/layout/components/header/MenuInner.tsx
@@ -0,0 +1,31 @@
+import {MenuItem} from './MenuItem'
+import {MenuInnerWithSub} from './MenuInnerWithSub'
+import {useIntl} from 'react-intl'
+
+export function MenuInner() {
+  const intl = useIntl()
+  return (
+    <>
+      <MenuItem title={intl.formatMessage({id: 'MENU.DASHBOARD'})} to='/dashboard' />
+      
+
+       
+
+      <MenuInnerWithSub title='Apps' to='/apps' menuPlacement='bottom-start' menuTrigger='click'>
+        {/* PAGES */}
+        <MenuInnerWithSub
+          title='Chat'
+          to='/apps/chat'
+          icon='/media/icons/duotune/communication/com012.svg'
+          hasArrow={true}
+          menuPlacement='right-start'
+          menuTrigger={`{default:'click', lg: 'hover'}`}
+        >
+          <MenuItem to='/apps/chat/private-chat' title='Private Chat' hasBullet={true} />
+          <MenuItem to='/apps/chat/group-chat' title='Group Chart' hasBullet={true} />
+          <MenuItem to='/apps/chat/drawer-chat' title='Drawer Chart' hasBullet={true} />
+        </MenuInnerWithSub>
+      </MenuInnerWithSub>
+    </>
+  )
+}
